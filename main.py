81f54a53020158f3205da5c30a9ba83682f7ef50
jerry <p.kobylarz@outlook.com>
2023-07-11 18:28:16 +0200

    requirements + calculation + simple chart

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+import matplotlib.pyplot as plt
+
+
 def read_datas():
-    """read all datas"""
-    h_start = float(input("Beginning height [ m ]: "))
-    v_start = float(input("Beginning speed [ m/s ]: "))
+    """read datas"""
+    def float_input(user_info, user_prompt, min_value):
+        print("---[ data loading ]---------")
+        print(user_info)
+        user_input = input(user_prompt)
+        if user_input.count(".") > 1:
+            return None
 
-    if h_start < 10:
-        print("Height is to low ( min 10[m] )!")
-        return None
+        if not user_input.replace(".", "").isdecimal():
+            return None
 
-    if v_start < 2:
-        print("Speed is to low ( min 2[m/s] )!")
-        return None
+        user_value = float(user_input)
+        if user_value < min_value:
+            print(f"Value {user_value} is lower that min value {min_value}!")
+            return None
+        return user_value
+
+    h_start = None
+    v_start = None
+
+    while h_start is None:
+        h_start = float_input(
+            "Wrong height data value, it should be float type ( 3.14 )",
+            "Write start height value [ m ]: ",
+            10,
+        )
+
+    while v_start is None:
+        v_start = float_input(
+            "Wrong speed data value, it should be float type ( 3.14 )",
+            "Write start speed value [ m/s ]: ",
+            2,
+        )
 
     return (h_start, v_start)
 
@@ -20,3 +45,22 @@ while initial_values is None:
     initial_values = read_datas()
 
 print("Data OK")
+
+# destroying tuple
+H_START, V_START = initial_values
+
+# calculating most important values
+g = 9.81  # m/s^2
+total_time = ((2 * H_START) / g) ** (1 / 2)
+max_range = V_START * total_time
+
+# generate chart, adding start point and finish point
+title = f"""Horizontal projection chart, V_START = {V_START} [m/s], ( g={g} m/s^2 ), 
+        flight time = {round(total_time, 4)} [s]"""
+
+plt.scatter(0, H_START, label=f"H_START={H_START} [m]")
+plt.scatter(max_range, 0, label=f"max_range={round(max_range, 3)} [m]")
+plt.grid()
+plt.title(title)
+plt.legend()
+plt.show()
